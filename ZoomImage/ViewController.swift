ff5ca849c912b0672f16450c08943807b81e5847
lakithjayalath <lakith54321@gmail.com>
2021-10-23 23:20:27 +0530

    zoom in and zoom out UIImageView

--- ZoomImage/ViewController.swift
+++ ZoomImage/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpScrollView()
     }
+    
+    func setUpScrollView() {
+        scrollView.delegate = self
+    }
+    
+}
 
-
+extension ViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
 
